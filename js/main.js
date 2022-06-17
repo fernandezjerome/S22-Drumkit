d5dc39381d5d61d624f0b5b39f05c2cd8a838704
fernandezjerome <97711003+fernandezjerome@users.noreply.github.com>
2022-06-17 16:08:08 -0400

    adding fadeout image

--- js/main.js
+++ js/main.js
@@ -2,29 +2,43 @@
 
 (() => {
     console.log('music player script file');
-    
-    function logkeyboardKeyCode(event){
-        //event is an object hat gets generated whenever the user press a keyboard key.
-        // it has lets of useful information inside of it (all about the event list)
-        
-        
-        //find the audio element that maches the keyboard keycode ( which one did the user press
 
-        //querSelector can take any valid CSS selector, including attributes
-        let targetAudio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    let theKeys = document.querySelectorAll('.key');
 
-        
-        //the if statemenet evaluats to true or false; if the condition is true, then the return statement is 
-        if(targetAudio === null) { return}
+    function logKeyboardKeyCode(event) {
+        // event is an object that gets generated whenever the user presses a keyboard key -> it has lots of useful information inside of it (all about the event itself)
 
-        //the ! is a NOT logical operator (look on Mon for the definition) --- if means targetAudio is null / doesn't exit
+        // find the audio element that matches the keyboard keyCode (which one did the user press?)
+
+        // querySelector can take any valid CSS selector, including attributes
+        let targetAudio = document.querySelector(`audio[data-key="${event.keyCode}"]`),
+            targetDiv = document.querySelector(`div[data-key="${event.keyCode}"]`);
+
+        // the return statement is like an ejector button. kills the code at this point, and nothing else will run
+
+        // the if statement evaluates to true or false; if the condition is true, then the return statement is executed which stops (exits) the function. Nothing else will run below that line.
+
+        // this will kill any errors, and make SURE that there's a matching audio element to play
+
+        // the ! is a NOT logical operator (look on MDN for the definition) -> it means targetAudio is null / doesn't exist
+        if (!targetAudio) { return }
+
         targetAudio.currentTime = 0;
         targetAudio.play();
 
-        //debugger;
+        // the classList is built-in JavaScript interface that lets you access the CSS classes on any element - you can add, remove, toggle CSS classes dynamically, which changes how they look on the page
+        targetDiv.classList.add('playing');
 
+        //debugger;
     }
 
-    // add some event handling for keyboard event
-    window.addEventListener('keyup', logkeyboardKeyCode)
+    // the 'this' keyword refers to the div that just finished transitioning (animating)
+    function removeHighlight() { this.classList.remove('playing'); }
+
+    // add some event handling for keyboard events
+    window.addEventListener('keyup', logKeyboardKeyCode);
+
+    // transitionend is fired whenever a CSS transition completes, which tells us that we can now remove the highlight we added when we pressed the keyboard key
+    theKeys.forEach(key => key.addEventListener('transitionend', removeHighlight));
+    
 })();
